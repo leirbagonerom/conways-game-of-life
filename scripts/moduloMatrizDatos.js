ed7d884b50c3be650998c115ae10bf97b215a6cb
leirbag <jgm223@hotmail.com>
2017-08-08 22:37:42 -0500

    creacion de modulos independientes

---
+++ scripts/moduloMatrizDatos.js
@@ -0,0 +1,23 @@
+var MatrizDatos = null;
+//ejemplo config { size: 12 }
+
+MatrizDatos = (function (config) {
+    var mod = {};
+    var config = {};
+    config.size = config.size || 12;
+
+    mod.matriz = [];
+    inicializar();
+
+    return mod;
+    
+    function inicializar() {
+        for (var i = 0; i < config.size; i++) {
+            var newCell = [];
+            for (var j = 0; j < config.size; j++)
+                newCell.push([i, j]);
+
+            mod.matriz.push(newCell);
+        }
+    }
+})(confObj);
